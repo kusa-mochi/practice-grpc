dc644c653c14778d25f6e3e03f357a3c0889aefc
kusa-mochi <whoatemyapplepie@gmail.com>
2023-01-21 22:13:13 +0900

    make a script to link to grpc interfaces.

---
+++ src/cmd/server/build.ps1
@@ -0,0 +1,15 @@
+$currentDirectory = (Convert-Path .)
+$scriptRoot = $PSScriptRoot
+
+cd $scriptRoot
+if(Test-Path go.mod) {
+    rm go.mod
+}
+if(Test-Path go.sum) {
+    rm go.sum
+}
+go mod init hello.com/server
+go mod edit -replace hello.com/hello=../../api/dist/go
+go mod tidy
+
+cd $currentDirectory
